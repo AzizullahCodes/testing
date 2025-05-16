0b79b9852517b8c0f9f0ead7bfc2986fd93a1033
AzizullahCodes <azizullahcodes@gmail.com>
2025-05-16 21:56:15 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,82 @@
+//  var myName = prompt('Enter my name ');
+//  if (myName === 'aziz')
+//     {console.log(myName,true);}
+// else{console.log(false);}
+
+ 
+ 
+//  var age = prompt('Enter your age');
+//         var education = prompt('Enter your education,like matric,middle,intermediate,ect');
+//         if (age >= 18 || education === 'matric' ){console.log('You are eligible for job');}
+//         else{console.log('You are not eligible for job');}
+// var flag = true;
+// if(!flag){console.log('Success');}
+// else{console.log('Not success');}
+
+//  var flag = true;
+//          if (flag !== true){console.log('Success');}
+//          else{console.log('Not success');}
+// var userName = prompt('Enter user name');
+// if (userName !== ''){var  askBreakFast = prompt('Kia khana hy');
+//         if (askBreakFast === 'tea' || askBreakFast === 'paratha'){console.log('Bring Breakfast')}
+//         else{console.log('nahi kahana')}
+// }
+// else {console.log('user is not available');}
+// var userName = prompt('Enter user name');
+// if (userName !== ''){var askBreakFast = prompt('kia khana hy');
+//         if(askBreakFast === 'tea' || askBreakFast === 'paratha'){console.log('Bring breakfast');}
+//         else {console.log('nhe khana');}
+// }
+// else {console.log('user is not available')}
+
+// var userName = prompt('Enter user name');
+// if (userName !== ''){var askBreakFast = prompt('Kia khana hy');
+//         if (askBreakFast === 'tea' || askBreakFast ==='egg'){console.log('bring breakfast');}
+//         else{console.log('nai khana');}
+// }
+// else{console.log('user is not available');}
+// var enter_marks = prompt('enter your obtained marks out of hundred');
+// if (enter_marks >= 50){var checkgrade = }
+//  var guest = prompt('enter guest name');
+//  if (guest !== ''){var askBreakFast = prompt('kia khana hy');
+//         if (askBreakFast === 'tea' || askBreakFast === 'bread'){console.log('Bring breakfast');}
+//         else{console.log('not bring something');}
+//  }
+//  else{console.log('guest is not here');}
+// var pin = +prompt('Enter your four digit pin');
+// if (pin === 4444) {var account_type =prompt('account type');
+//          if(account_type === 'saving' || account_type === 'current');
+//          {}
+        
+// }
+
+
+
+// else{console.log('pin is not correct');}
+// var number = prompt('enter number');
+// var remainder = number % 2;
+// console.log(remainder)
+// if( remainder === 0){if (number)}  
+
+
+
+// else{console.log('no is odd');}
+
+// var number = prompt('Enter a number');
+// number = Number(number);
+// if (number % 2 === 0){if (number > 10){console.log('even and greater than 10');}
+// else{console.log('even and 10 or less than 10');}}
+// else{if (number > 10){'Odd and greater than 10'}else{console.log('odd and 10 or less');}}
+// var age = prompt('enter your age');
+// if (age >= 18){var gender = prompt('Enter your gender,male or female');
+//         if(gender === 'male'){console.log('you are a man');}
+//         else{console.log('you are female');}
+// }
+
+// else{console.log('you are less than 18 years');}
+ var age = prompt('Enter your age');
+         if (age >= 18) {var id_card = prompt('do you have an id card,enter yes or no');
+                if (id_card === 'yes'){console.log('Welcome! use your right of voting to suitable candidate')}
+                else{console.log('You have no id card, id card is must for voting');}
+         }
+         else{console.log('You can not vote becaues your age is less');}
