1f55f390fc65cc78788d9907291acb9a83213b91
AzizullahCodes <azizullahcodes@gmail.com>
2025-06-10 21:22:04 +0500

    ufdg

--- app.js
+++ app.js
@@ -1,126 +1,374 @@
-//  var myName = prompt('Enter my name ');
-//  if (myName === 'aziz')
-//     {console.log(myName,true);}
-// else{console.log(false);}
+// // // //  var myName = prompt('Enter my name ');
+// // // //  if (myName === 'aziz')
+// // // //     {console.log(myName,true);}
+// // // // else{console.log(false);}
 
  
  
-//  var age = prompt('Enter your age');
-//         var education = prompt('Enter your education,like matric,middle,intermediate,ect');
-//         if (age >= 18 || education === 'matric' ){console.log('You are eligible for job');}
-//         else{console.log('You are not eligible for job');}
-// var flag = true;
-// if(!flag){console.log('Success');}
-// else{console.log('Not success');}
+// // // //  var age = prompt('Enter your age');
+// // // //         var education = prompt('Enter your education,like matric,middle,intermediate,ect');
+// // // //         if (age >= 18 || education === 'matric' ){console.log('You are eligible for job');}
+// // // //         else{console.log('You are not eligible for job');}
+// // // // var flag = true;
+// // // // if(!flag){console.log('Success');}
+// // // // else{console.log('Not success');}
 
-//  var flag = true;
-//          if (flag !== true){console.log('Success');}
-//          else{console.log('Not success');}
-// var userName = prompt('Enter user name');
-// if (userName !== ''){var  askBreakFast = prompt('Kia khana hy');
-//         if (askBreakFast === 'tea' || askBreakFast === 'paratha'){console.log('Bring Breakfast')}
-//         else{console.log('nahi kahana')}
-// }
-// else {console.log('user is not available');}
-// var userName = prompt('Enter user name');
-// if (userName !== ''){var askBreakFast = prompt('kia khana hy');
-//         if(askBreakFast === 'tea' || askBreakFast === 'paratha'){console.log('Bring breakfast');}
-//         else {console.log('nhe khana');}
-// }
-// else {console.log('user is not available')}
+// // // //  var flag = true;
+// // // //          if (flag !== true){console.log('Success');}
+// // // //          else{console.log('Not success');}
+// // // // var userName = prompt('Enter user name');
+// // // // if (userName !== ''){var  askBreakFast = prompt('Kia khana hy');
+// // // //         if (askBreakFast === 'tea' || askBreakFast === 'paratha'){console.log('Bring Breakfast')}
+// // // //         else{console.log('nahi kahana')}
+// // // // }
+// // // // else {console.log('user is not available');}
+// // // // var userName = prompt('Enter user name');
+// // // // if (userName !== ''){var askBreakFast = prompt('kia khana hy');
+// // // //         if(askBreakFast === 'tea' || askBreakFast === 'paratha'){console.log('Bring breakfast');}
+// // // //         else {console.log('nhe khana');}
+// // // // }
+// // // // else {console.log('user is not available')}
 
-// var userName = prompt('Enter user name');
-// if (userName !== ''){var askBreakFast = prompt('Kia khana hy');
-//         if (askBreakFast === 'tea' || askBreakFast ==='egg'){console.log('bring breakfast');}
-//         else{console.log('nai khana');}
-// }
-// else{console.log('user is not available');}
-// var enter_marks = prompt('enter your obtained marks out of hundred');
-// if (enter_marks >= 50){var checkgrade = }
-//  var guest = prompt('enter guest name');
-//  if (guest !== ''){var askBreakFast = prompt('kia khana hy');
-//         if (askBreakFast === 'tea' || askBreakFast === 'bread'){console.log('Bring breakfast');}
-//         else{console.log('not bring something');}
-//  }
-//  else{console.log('guest is not here');}
-// var pin = +prompt('Enter your four digit pin');
-// if (pin === 4444) {var account_type =prompt('account type');
-//          if(account_type === 'saving' || account_type === 'current');
-//          {}
+// // // // var userName = prompt('Enter user name');
+// // // // if (userName !== ''){var askBreakFast = prompt('Kia khana hy');
+// // // //         if (askBreakFast === 'tea' || askBreakFast ==='egg'){console.log('bring breakfast');}
+// // // //         else{console.log('nai khana');}
+// // // // }
+// // // // else{console.log('user is not available');}
+// // // // var enter_marks = prompt('enter your obtained marks out of hundred');
+// // // // if (enter_marks >= 50){var checkgrade = }
+// // // //  var guest = prompt('enter guest name');
+// // // //  if (guest !== ''){var askBreakFast = prompt('kia khana hy');
+// // // //         if (askBreakFast === 'tea' || askBreakFast === 'bread'){console.log('Bring breakfast');}
+// // // //         else{console.log('not bring something');}
+// // // //  }
+// // // //  else{console.log('guest is not here');}
+// // // // var pin = +prompt('Enter your four digit pin');
+// // // // if (pin === 4444) {var account_type =prompt('account type');
+// // // //          if(account_type === 'saving' || account_type === 'current');
+// // // //          {}
         
-// }
+// // // // }
 
 
 
-// else{console.log('pin is not correct');}
-// var number = prompt('enter number');
-// var remainder = number % 2;
-// console.log(remainder)
-// if( remainder === 0){if (number)}  
+// // // // else{console.log('pin is not correct');}
+// // // // var number = prompt('enter number');
+// // // // var remainder = number % 2;
+// // // // console.log(remainder)
+// // // // if( remainder === 0){if (number)}  
 
 
 
-// else{console.log('no is odd');}
+// // // // else{console.log('no is odd');}
 
-// var number = prompt('Enter a number');
-// number = Number(number);
-// if (number % 2 === 0){if (number > 10){console.log('even and greater than 10');}
-// else{console.log('even and 10 or less than 10');}}
-// else{if (number > 10){'Odd and greater than 10'}else{console.log('odd and 10 or less');}}
-// var age = prompt('enter your age');
-// if (age >= 18){var gender = prompt('Enter your gender,male or female');
-//         if(gender === 'male'){console.log('you are a man');}
-//         else{console.log('you are female');}
-// }
+// // // // var number = prompt('Enter a number');
+// // // // number = Number(number);
+// // // // if (number % 2 === 0){if (number > 10){console.log('even and greater than 10');}
+// // // // else{console.log('even and 10 or less than 10');}}
+// // // // else{if (number > 10){'Odd and greater than 10'}else{console.log('odd and 10 or less');}}
+// // // // var age = prompt('enter your age');
+// // // // if (age >= 18){var gender = prompt('Enter your gender,male or female');
+// // // //         if(gender === 'male'){console.log('you are a man');}
+// // // //         else{console.log('you are female');}
+// // // // }
 
-// else{console.log('you are less than 18 years');}
-//  var age = prompt('Enter your age');
-//          if (age >= 18) {var id_card = prompt('do you have an id card,enter yes or no');
-//                 if (id_card === 'yes'){console.log('Welcome! use your right of voting to suitable candidate')}
-//                 else{console.log('You have no id card, id card is must for voting');}
-//          }
-//          else{console.log('You can not vote becaues your age is less');}
+// // // // else{console.log('you are less than 18 years');}
+// // // //  var age = prompt('Enter your age');
+// // // //          if (age >= 18) {var id_card = prompt('do you have an id card,enter yes or no');
+// // // //                 if (id_card === 'yes'){console.log('Welcome! use your right of voting to suitable candidate')}
+// // // //                 else{console.log('You have no id card, id card is must for voting');}
+// // // //          }
+// // // //          else{console.log('You can not vote becaues your age is less');}
 
 
 
-//          var q1 = prompt("Enter Your Number (To Check If It Is Divisible By 3)");
+// // // //          var q1 = prompt("Enter Your Number (To Check If It Is Divisible By 3)");
 
-// if (q1 % 3 == 0) {
-//     console.log("It's Divisible By 3");
-// }
-// else if  (q1 == 0 || q1 < 0){console.log('no ');}
-// else{console.log('No is not divisible by 3')}
-//     var yourName = prompt("Enter Your Name (To Check If You Have The Same Name As Me")
-// var myName = 'Aziz';
-// if (yourName === myName) {
-//     console.log("Hello Same Name Person")
-// }
+// // // // if (q1 % 3 == 0) {
+// // // //     console.log("It's Divisible By 3");
+// // // // }
+// // // // else if  (q1 == 0 || q1 < 0){console.log('no ');}
+// // // // else{console.log('No is not divisible by 3')}
+// // // //     var yourName = prompt("Enter Your Name (To Check If You Have The Same Name As Me")
+// // // // var myName = 'Aziz';
+// // // // if (yourName === myName) {
+// // // //     console.log("Hello Same Name Person")
+// // // // }
 
-// else {
-//     console.log("Hello Stranger")
+// // // // else {
+// // // //     console.log("Hello Stranger")
 
-// }
+// // // // }
 
-// var array = ['aziz','mubin',98,true, false]
-// console.log(array);
+// // // // var array = ['aziz','mubin',98,true, false]
+// // // // console.log(array);
 
-// var name = 'sqlainhaider';
-// console.log(name.length);
+// // // // var name = 'sqlainhaider';
+// // // // console.log(name.length);
 
-// var user  = 'john';
-// var employees = ['ahmed','bilal', '','asad',user]
-// console.log(employees); 
+// // // // var user  = 'john';
+// // // // var employees = ['ahmed','bilal', '','asad',user]
+// // // // console.log(employees); 
 
-//  var array = ['ahmed', 'laptop','cat','asad','company'];
-//  var length = array.length;
-//  console.log(length); 
-//   var friends = ['ahmed','faraz','asad'];
-//   var userInput = prompt('enter your friend name');
-//   if (userInput === friends[0]||
-//     userInput === friends[1]||
-//     userInput === friends [2]
-//   ){console.log('friends found');}
-//   else{console.log('friend not found');}
-var skills = ['html5','css3','bootstrap'];
-var bioData = ['john', 30, 'Male', 'Abc Company', skills]
-var nd      = prompt ('is skills name array in bio data');
+// // // //  var array = ['ahmed', 'laptop','cat','asad','company'];
+// // // //  var length = array.length;
+// // // //  console.log(length); 
+// // // //   var friends = ['ahmed','faraz','asad'];
+// // // //   var userInput = prompt('enter your friend name');
+// // // //   if (userInput === friends[0]||
+// // // //     userInput === friends[1]||
+// // // //     userInput === friends [2]
+// // // //   ){console.log('friends found');}
+// // // //   else{console.log('friend not found');}
+// // // var skill = ['html','css','javascript']; 
+// // // var bio = ['sadoon',11,skill];
+// // // var user = prompt('enter the skill');
+// // // if ('object' == typeof bio[0] || 'object' == typeof bio[1]|| 'object' == typeof bio[2])
+// // // {if(user == bio[0][0]||
+// // //     user == bio[0][1] ||
+// // //     user == bio[0][2] ||
+// // //     user == bio[1][0] ||
+// // //     user == bio[1][1] ||
+// // //     user == bio[1][2] ||
+// // //     user == bio[2][0] ||
+// // //     user == bio[2][1] ||
+// // //     user == bio[2][2] ||
+// // // ){console.log('your data is true')}
+// // // else{console.log('your data is false');}}
+// // // else {console.log('the skill is not found');}
+
+// // // var arr = ['']; 
+// // // var array = ['a','b','c','d','e','f','g'];
+// // // var spl   = array.splice(3,3,'fsf');
+// // // console.log(spl)
+
+// // // var arr1 = [1,2,3];
+// // // var arr2 = [3,4,5];
+// // // var arr3 = [6,7,9]
+// // // console.log(arr1.concat(arr2,arr3))
+
+// // // var fruits = ['apple','bnana','orange'];
+// // // var remove = fruits.splice(1,2,'grapes');
+// // // console.log(fruits);
+// // // console.log(remove);
+// // // var arr1 = ['apple','mango'];
+// // // var arr2 = [false,true];
+// // // var arr3 = [43,12,89,];
+// // // var newArray = arr1.concat(arr2,arr3);
+// // // console.log(newArray);
+
+// // // var arr = [10,20,30,40,50,60];
+// // // var newarr = arr.slice(1,4);
+// // // console.log(newarr);
+// // // console.log(newarr)
+
+// // // var array = [[1,2],[3,4],[5,6]];
+// // // // array1.concat(array2, array3, ..., arrayN)
+// // // var newArray = array[0].concat(array[1],array[2]);
+// // // console.log(newArray)
+// // // var h = 'i am working'
+// // // document.write(h);
+// // // var num = [1,4,112,9,222,9,8];
+// // // console.log(num);
+// // // num.sort();
+// // // console.log(num)
+// // // var array = ['chips','cake','snacks','choclate','biscuits'];
+// // // var ask = prompt('what are you searching');
+// // // var ind = array.indexOf(ask);
+// // // if(ind !== -1){console.log('available')}
+// // // else{console.log('not available')}
+// // // var ab = ['c','b','g','a']
+// // // ab.sort();
+// // // console.log(ab)
+// // //  var array = ['chips','cake','snacks','choclate','biscuits'];
+// // //  var ask = prompt('enter you are finding')
+// // //  var isavailable = false
+// // // for(var i = 0; i < array.length; i++){if(ask === array[i])
+// // //    { isavailable = true;
+// // // break;}
+// // // }
+// // // if(isavailable){console.log('available')}
+// // // else{console.log('not availabel')}
+// // // var a = [];
+// // // console.log(a);
+// // // console.log(typeof a);
+// // // var a = 5;
+// // // a++;
+// // // console.log(a)
+// // alert("Hello" + ' ' + "World");
+// // var score = 85;
+// // if (score >= 90) {
+// //   console.log("A");
+// // } else if (score >= 80) {
+// //   console.log("B");
+// // } else {
+// //   console.log("C");
+// // }
+// // var age = 25, gender = "male";
+// // if (age > 18 && gender === "male") {
+// //   console.log("Allowed");
+// // }
+// // var x = 10;
+// // if (x > 5) {
+// //   if (x < 20) {
+// //     console.log("Between 5 and 20");
+// //   }
+// // }
+// // let fruits = ['banana','apple','lemon','orange'];
+// // let nnn = fruits.slice(0,);
+// // console.log(nnn)
+// // for(var i = 1; i<= 5; i++){console.log('hellow')}
+// // let fruits = ['apple','orange','banana'];
+// // let checkFruit = prompt('enter the name of fruit you are finding');
+// // if(checkFruit === fruits[0]){console.log('fruit is available')}
+// // else if(checkFruit === fruits[1]){console.log('fruit is available')}
+// // else if(checkFruit === fruits[2]){console.log('fruit is available')}
+// // else{console.log('fruit is not available')}
+// // let fruits = ['apple','orange','banana'];
+// //  let checkFruit = prompt('enter the name of fruit you are finding');
+// //  var isavailable = false;
+// //  for(var i = 0 ; i <= fruits.length; i++){if(checkFruit === fruits[i]){
+// //     isavailable = true;
+// //     break;
+// //  }}
+// //  if(isavailable === true){console.log('fruit is available')}
+// //  else{console.log('fruit is  not available')}
+// // var pi = Math.PI;
+// // console.log(pi);
+// // console.log(pi.toFixed(2));
+// // console.log(Math.round(4))
+// // console.log(Math.round(5.2))
+// // console.log(Math.round(6.5))
+// // console.log(Math.round(9.9))
+// // Math.sqrt(9);
+// // Math.sqrt(49);
+// // Math.sqrt(81)
+// // console.log(Math.sqrt(81));
+// // console.log(Math.sqrt(-23))
+// // console.log(Math.sqrt(0))
+// // console.log(Math.abs(2));
+// // console.log(Math.abs(-4));
+// // console.log(Math.abs(-22));
+// // console.log(Math.abs(-49));
+// // console.log(Math.ceil(4))
+// // console.log(Math.ceil(4.4))
+// // console.log(Math.ceil(1.2))
+// // console.log(Math.ceil(14.6))
+// // console.log(Math.ceil(30.6))
+// // console.log(Math.floor(2));
+// // console.log(Math.floor(3.142));
+// // console.log(Math.floor(20.144));
+
+// // console.log(Math.floor(2.9));
+
+// // console.log(Math.floor(2.9));
+// // console.log(Math.min(4,1,888,-2,-888,-8,22,31,-23))
+// // var randomVal = Math.random();
+// // console.log((randomVal.toFixed(2)*5));
+// // var randomVal = Math.random();
+// // console.log(randomVal)
+// // var dice = Math.ceil(Math.random()* 6);
+// // console.log(dice);
+// // var friend = ['john','kim','mark','andrew','trish'];
+// // var randomVal = Math.floor(Math.random()*friend.length);
+// // console.log(friend[randomVal])
+// // var today = new Date();
+// // var bday = new Date('08 july 1996');
+// // // console.log(today);
+// // // console.log(bday);
+// // var todayInMs = today.getTime();
+// // var bdayInMs = bday.getTime();
+// // var calculateMs = todayInMs - bdayInMs;
+// // console.log(calculateMs)
+// // // console.log(todayInMs);
+// // // console.log(bdayInMs);
+// // var dayFormula = calculateMs /(1000 * 60 * 60 * 24);
+// //     dayFormula = Math.floor(dayFormula);
+// // console.log(dayFormula)
+
+
+// // var today = new Date();
+// // var classTime  = new Date('12 june 2025 09:00:00');
+// // // console.log(today);
+// // // console.log(bday);
+// // var todayInMs = today.getTime();
+// // var classTime = classTime.getTime();
+// // var calculateMs = classTime - todayInMs;
+// // console.log(calculateMs)
+// // // console.log(todayInMs);
+// // // console.log(bdayInMs);
+// // var hourFormula = calculateMs /(1000 * 60 * 60);
+// //     hourFormula = Math.floor(hourFormula);
+// // console.log(hourFormula)
+// var rightNow = new Date();
+// //    rightNow = rightNow.setDate('15');
+// rightNow.setFullYear(2025)
+//    console.log(rightNow)
+//    // js-practice-assignment 25
+// // ___________________________________________________________
+// // ques no 1
+// // How can we render date on browser?
+// // _____________ 
+// //  ans 
+// // var rightNow = Date();
+// // console.log(rightNow);
+// // console.log(typeof rightNow);
+// // var rightNow = new Date();
+// // console.log(rightNow);
+// // console.log(typeof rightNow);
+// // var dat = Date();
+// // console.log(dat);
+// // console.log(typeof dat);
+// // var dat = new Date();
+// // console.log(dat);
+// // console.log(typeof dat)
+// // var rightNow = new Date();
+// // var year = rightNow.getFullYear();
+// // console.log(year);
+// // var month = rightNow.getMonth();
+// // console.log(month);
+// // var day = rightNow.getDay(); 
+// // console.log(day);
+// // var dt = rightNow.getDate();
+// // console.log(dt);
+// // var hour = rightNow.getHours();
+// // console.log(hour)
+// // var minutes = rightNow.getMinutes();
+// // console.log(minutes);
+// // var seconds = rightNow.getSeconds();
+// // console.log(seconds);
+// // var miliseconds = rightNow.getMilliseconds();
+// // console.log(miliseconds);
+// // var time = rightNow.getTime();
+// // console.log(time);
+// // console.log(rightNow.toDateString());
+// // var tim = rightNow.toTimeString();
+// // console.log(tim)
+// // var local = rightNow.toLocaleTimeString();
+// // console.log(local)
+// // var localdate = rightNow.toLocaleDateString();
+// // console.log(localdate)
+// // var birthday = new Date('08 July 1996');
+// // console.log(birthday)
+
+// // // var rightNow = new Date();
+// // // let array = ['sunday','monday','tuesday','wednesday','thursday','friday','satruday'];
+// // // var dayName = rightNow.getDay();
+// // // console.log(dayName);
+// // // console.log(array[rightNow.getDay()])
+// // // var result = rightNow.getDay(array[i]);
+// // // console.log(result)
+
+// // var dayName = ['sunday','monday','tuesday','wednesday','thursday','friday','satruday'];
+// // var rightNow = new Date();
+// // let result = dayName[rightNow.getDay()];
+// // console.log(result)
+
+// // var userInput = Number(prompt('enter your birth year'));
+// // console.log(rightNow.getFullYear()- userInput);
+// var rightNow = new Date();
+// var today = new Date();
+// var bday = new Date('08 july 1996');
+// console.log(today);
+// console.log(bday);
